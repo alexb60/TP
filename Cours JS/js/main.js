b7adfb11665c055fac2555e7a9b38521c5d9900d
alexb60 <alexandrebertrand60@yahoo.fr>
2022-02-09 21:52:44 +0100

    SASS + JS 09 02 2022 2152

---
+++ Cours JS/js/main.js
@@ -0,0 +1,18 @@
+// alert("Cours JS");
+const jour = "mercredi";
+const date = 09;
+
+let age = 21;
+
+console.log(age);
+
+console.log("Mon age est : " + age);
+
+let adresse = "45 Boulevard Paul Verley";
+console.log("Mon adresse est : " + adresse + " - Dunkerque");
+console.log(`Mon adresse est : ${adresse} - Dunkerque`);
+
+let adulte = 18;
+let retraite = 62;
+
+console.log(`L'âge adulte est à ${adulte} ans, l'âge de la retraite est à ${retraite} ans.`);
