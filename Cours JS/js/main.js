9d8fe5efa89126dc51617a8a7d3ccdf4c6f92a7e
alexb60 <alexandrebertrand60@yahoo.fr>
2022-02-16 16:48:04 +0100

    16 02 2022 16 47

--- Cours JS/js/main.js
+++ Cours JS/js/main.js
@@ -175,28 +175,28 @@
 
 // affichageDate();
 
-const todos = [
-  {
-    id: 1,
-    text: "Faire les courses",
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    text: "Balade au vieux Lille",
-    isCompleted: true,
-  },
-  {
-    id: 3,
-    text: "Préparer le diner",
-    isCompleted: false,
-  },
-  {
-    id: 4,
-    text: "Regarder la TV",
-    isCompleted: false,
-  },
-];
+// const todos = [
+//   {
+//     id: 1,
+//     text: "Faire les courses",
+//     isCompleted: true,
+//   },
+//   {
+//     id: 2,
+//     text: "Balade au vieux Lille",
+//     isCompleted: true,
+//   },
+//   {
+//     id: 3,
+//     text: "Préparer le diner",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 4,
+//     text: "Regarder la TV",
+//     isCompleted: false,
+//   },
+// ];
 
 // todos.map((todo) =>
 //   todo.isCompleted
@@ -213,34 +213,399 @@ const todos = [
 //   {
 //     id: 1,
 //     marque: "Mercedes",
-//     puissances: 10,
+//     puissance: 10,
 //   },
 //   {
 //     id: 2,
 //     marque: "Renault",
-//     puissances: 8,
+//     puissance: 8,
 //   },
 //   {
 //     id: 3,
 //     marque: "BMW",
-//     puissances: 12,
+//     puissance: 12,
 //   },
 //   {
 //     id: 4,
 //     marque: "Ferrari",
-//     puissances: 30,
+//     puissance: 30,
 //   },
 //   {
 //     id: 5,
 //     marque: "Maserati",
-//     puissances: 20,
+//     puissance: 20,
 //   },
 // ];
 
 // voitures.map((voiture) => {
-//   document.write(voiture.puissances >= 10 ? `${voiture.marque} : ${voiture.puissances} ch <br />` : "");
+//   document.write(voiture.puissance >= 10 ? `${voiture.marque} : ${voiture.puissance} ch <br />` : "");
 // });
 
-for (let i = 0; i < todos.length; i++) {
-  document.write(`Tâche n°${todos[i].id} : ${todos[i].text} - ${todos[i].isCompleted ? "terminée" : "non terminée"} <br />`);
-}
+// for (let i = 0; i < todos.length; i++) {
+//   document.write(`Tâche n°${todos[i].id} : ${todos[i].text} - ${todos[i].isCompleted ? "terminée" : "non terminée"} <br />`);
+// }
+
+// let nbCases = 100;
+// function remplirTableau(nbCases) {
+//   let tab = [];
+//   for (let i = 0; i < nbCases; i++) {
+//     tab[i] = i + 1;
+//   }
+//   return tab;
+// }
+
+// function afficherTabPair(tab) {
+//   for (let i = 0; i < tab.length; i++) {
+//     tab[i] % 2 == 0 ? document.write(`${tab[i]}, `) : "";
+//   }
+// }
+
+// afficherTabPair(remplirTableau(100));
+
+// document.write("<br />");
+
+// function afficherValeur(tab, valeur) {
+//   for (let i = 0; i < tab.length; i++) {
+//     if (tab[i] == valeur) {
+//       break;
+//     }
+//     document.write(`${tab[i]}, `);
+//   }
+// }
+
+// afficherValeur(remplirTableau(100), 50);
+
+// let marquesVoitures = [
+//   "Abarth",
+//   "Ac",
+//   "Acura",
+//   "Aixam",
+//   "Alfa Romeo",
+//   "Alke",
+//   "Allard",
+//   "Alpina",
+//   "Alpine",
+//   "Alvis",
+//   "AMC",
+//   "Amilcar",
+//   "Arash",
+//   "Ariel",
+//   "ARO",
+//   "Ashok Leyland",
+//   "Aston Martin",
+//   "Auburn",
+//   "Audi",
+//   "Austin",
+//   "Austin Healey",
+//   "Auto Union",
+//   "Autobianchi",
+//   "AvtoGAZ",
+//   "AvtoVAZ",
+//   "Bellier",
+//   "Bentley",
+//   "BMW",
+//   "Bolloré",
+//   "Bristol",
+//   "Bugatti",
+//   "Buick",
+//   "Cadillac",
+//   "Casalini",
+//   "Caterham",
+//   "Chatenet",
+//   "Chausson",
+//   "Chenard et Walcker",
+//   "Chevrolet",
+//   "Chrysler",
+//   "Citroën",
+//   "Dacia",
+//   "Daewoo",
+//   "Daihatsu",
+//   "Datsun",
+//   "De Dion-Bouton",
+//   "De Tomaso",
+//   "Delage",
+//   "Delahaye",
+//   "DeLorean",
+//   "Dodge",
+//   "Donkervoort",
+//   "DS",
+//   "Dué",
+//   "Eagle",
+//   "Exagon",
+//   "Excalibur",
+//   "Facel Vega",
+//   "Ferrari",
+//   "FIAT",
+//   "Fisker",
+//   "Ford",
+//   "Ford Mustang",
+//   "Fuso",
+//   "GAZ",
+//   "Ginetta",
+//   "GMC",
+//   "Grecav",
+//   "Gumpert",
+//   "Hispano Suiza",
+//   "Hommell",
+//   "Honda",
+//   "Horch",
+//   "Hummer",
+//   "Hyundai",
+//   "Infiniti",
+//   "Innocenti",
+//   "Isuzu",
+//   "Iveco",
+//   "Jaguar",
+//   "JDM Simpa",
+//   "Jeep",
+//   "Karma",
+//   "Kia",
+//   "Koenigsegg",
+//   "KTM",
+//   "Lada",
+//   "Lamborghini",
+//   "Lancia",
+//   "Land Rover",
+//   "Lexus",
+//   "Ligier",
+//   "Lincoln",
+//   "Lotus",
+//   "Maserati",
+//   "Matra",
+//   "Maybach",
+//   "Mazda",
+//   "McLaren",
+//   "Mega",
+//   "Mercedes-Benz",
+//   "MG",
+//   "Mia electric",
+//   "Mini",
+//   "Mitsubishi",
+//   "Mitsuoka",
+//   "Monica",
+//   "Morgan",
+//   "Morris",
+//   "Nash",
+//   "Neckar",
+//   "Nissan",
+//   "Noble",
+//   "NSU",
+//   "Oldsmobile",
+//   "Opel",
+//   "Packard",
+//   "Pagani",
+//   "Panhard",
+//   "Panoz",
+//   "Panther",
+//   "Peugeot",
+//   "PGO",
+//   "Plymouth",
+//   "Pontiac",
+//   "Porsche",
+//   "Radical",
+//   "Renault",
+//   "Riley",
+//   "Rolls-Royce",
+//   "Rover",
+//   "Saab",
+//   "Saleen",
+//   "Santana",
+//   "Seat",
+//   "Secma",
+//   "Shelby",
+//   "Simca",
+//   "Skoda",
+//   "Smart",
+//   "SsangYong",
+//   "Studebaker",
+//   "Subaru",
+//   "Suzuki",
+//   "Talbot",
+//   "Tata",
+//   "Tazzari",
+//   "Tesla Motors",
+//   "Toyota",
+//   "Triumph",
+//   "Tvr",
+//   "Ultima",
+//   "VAZ",
+//   "Vector",
+//   "Venturi",
+//   "Volkswagen",
+//   "Volvo",
+//   "Weber",
+//   "Wiesmann",
+//   "Wolseley",
+//   "Zastava",
+//   "Zenvo",
+// ];
+
+// function rechercherMarque(tabMarque, marque) {
+//   let position = -1;
+//   for (let i = 0; i < tabMarque.length; i++) {
+//     if (tabMarque[i] == marque) {
+//       position = i + 1;
+//       break;
+//     }
+//   }
+//   return -1;
+//   document.write(`La marque ${marque} ${position != -1 ? `est dans la ${position}e case du tableau` : `n'a pas été trouvée`}.`);
+// }
+
+// let marque = "Tata";
+// document.write(rechercherMarque(marquesVoitures, marque));
+
+// parcourir le tableau
+//   si marque = valeur donnée
+//      alors position = index +1
+//  finSi
+// si position != -1
+//       alors marque trouvée
+// sinon marque non trouvée
+// retourner position
+
+// CORRECTION MARQUES DE VOITURE AVEC FOR
+// function rechercheVoiture(val, tab) {
+//   for (let i = 0; i < tab.length; i++) {
+//     if (tab[i] === val) {
+//       return i + 1;
+//     }
+//   }
+//   return -1;
+// }
+
+// let marque = prompt("Marque de voiture");
+// let position = rechercheVoiture(marque, voitures);
+// let msg = position !== -1 ? `existe à la position ${position}` : `n'existe pas`;
+// document.write(`La voiture ${marque} ${msg}`);
+// document.write(`La voiture ${marque} ${position !== -1 ? `existe à la position ${position}` : `n'existe pas`}`);
+
+// function rechercherMarque2(tabMarque, marque) {
+//   let position2 = -1;
+//   let i = 0;
+//   while (position2 == -1 && i < tabMarque.length) {
+//     if (tabMarque[i] == marque) {
+//       position2 = i + 1;
+//     }
+//     i++;
+//   }
+//   document.write(`La marque ${marque} ${position2 != -1 ? `est dans la ${position2}e case du tableau` : `n'a pas été trouvée`}.`);
+// }
+
+// let marque2 = "Tata";
+// rechercherMarque2(marquesVoitures, marque2);
+
+let voitures = [
+  {
+    id: 1,
+    marque: "Mercedes",
+    puissance: 10,
+  },
+  {
+    id: 2,
+    marque: "Renault",
+    puissance: 8,
+  },
+  {
+    id: 3,
+    marque: "BMW",
+    puissance: 12,
+  },
+  {
+    id: 4,
+    marque: "Ferrari",
+    puissance: 30,
+  },
+  {
+    id: 5,
+    marque: "Maserati",
+    puissance: 20,
+  },
+];
+
+// let voituresPuissantes = voitures.filter((voiture) => {
+//   return voiture.puissance >= 10;
+// });
+
+// // console.log(voituresPuissantes);
+
+// function filtreVoitures(tab) {
+//   let voituresFiltrees = [];
+//   let i = 0;
+//   let j = 0;
+//   while (i < tab.length) {
+//     if (tab[i].puissance >= 10) {
+//       voituresFiltrees[j] = tab[i];
+//       j++;
+//     }
+//     i++;
+//   }
+//   return voituresFiltrees;
+// }
+
+// console.log(filtreVoitures(voitures));
+
+// Initialisation nouveauTableau
+// Initialisation i à 0
+// Initialisation j à 0
+// Tant que i est inférieur à la taille du tableau
+//     Faire
+//         Si puissance est supérieure ou égale à 10
+//           Alors
+//              nouveauTableau [j] est égal à tableau[i]
+//              j++
+//         finSi
+//         i++
+// finTantQue
+// retourner nouveauTableau
+
+// let i = 0;
+// while (i < voitures.length) {
+//   document.write(voitures[i].puissance >= 10 ? `${voitures[i].marque} : ${voitures[i].puissance} ch <br />` : "");
+//   i++;
+// }
+
+// const todos = [
+//   {
+//     id: 1,
+//     text: "Faire les courses",
+//     isCompleted: true,
+//   },
+//   {
+//     id: 2,
+//     text: "Balade au vieux Lille",
+//     isCompleted: true,
+//   },
+//   {
+//     id: 3,
+//     text: "Préparer le diner",
+//     isCompleted: false,
+//   },
+//   {
+//     id: 4,
+//     text: "Regarder la TV",
+//     isCompleted: false,
+//   },
+// ];
+
+// todos.forEach(function (todo) {
+//   document.write(todo.text + "<br>");
+// });
+
+// document.write("<br />");
+
+// todos.forEach((todo) => {
+//   document.write(todo.text + "<br>");
+// });
+
+// voitures.forEach((voiture) => {
+//   document.write(`${voiture.id} - ${voiture.marque} : ${voiture.puissance} ch <br />`);
+// });
+
+voitures
+  .filter((voiture) => {
+    return voiture.puissance >= 10;
+  })
+  .map((voiture) => {
+    document.write(`${voiture.id}- ${voiture.marque} : ${voiture.puissance} <br>`);
+  });
