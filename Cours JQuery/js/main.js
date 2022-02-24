d67215bacc1610e7a97ecff129b9927670183158
alexb60 <alexandrebertrand60@yahoo.fr>
2022-02-24 18:19:48 +0100

    24 02 2022 18 19

--- Cours JQuery/js/main.js
+++ Cours JQuery/js/main.js
@@ -25,5 +25,41 @@
 // $("li:nth-child(2)").next().css({ color: "green", border: "2px solid green" });
 // $("li:nth-child(2)").prev().css({ color: "blue", border: "2px solid blue" });
 
-$(".para-dom").next().css({ color: "green", border: "2px solid green" });
-$(".para-dom").prev().css({ color: "blue", border: "2px solid blue" });
+// $(".para-dom").next().css({ color: "green", border: "2px solid green" });
+// $(".para-dom").prev().css({ color: "blue", border: "2px solid blue" });
+
+// $("form input").last().css({ color: "white", background: "skyblue", "border-radius": "5px", padding: "10px" });
+
+// $(".para-dom").next().siblings().css("color", "red");
+
+// $("[for='capitale']").text("Le chef-lieu du Pas-de-Calais");
+
+// $(document).ready(function () {
+//   $(".test-btn").click(function () {
+//     alert("test bouton");
+//   });
+// });
+
+// $(".dom [type='submit']").click(function () {
+//   if ($(".dom #capitale").val() == "paris") {
+//     $(".reponse").text("Votre réponse est correcte");
+//     $(".resultat").text("10/10");
+//   } else {
+//     $(".reponse").text("Réponse erronée").css({ color: "red", border: "solid 1px red" });
+//     $(".resultat").text("0/10").css("color", "red");
+//   }
+// });
+
+// $(".extensible").prepend("contenu d'extension au debut<br/>");
+
+// $(".para-dom").prepend("<h5>Titre de niveau 5</h5>");
+
+// $(".para-dom span").append("<ul><li></li><li></li></ul>");
+
+// $("ul").before(`<ul id="ul1"><li><a href="https://www.google.fr" target="_blank">Lien vers Google</a></li></ul>`);
+
+// $(".para-dom").after(`<h1>${$("label").text()}</h1>`);
+
+// $(".supprimer").remove();
+
+console.log($(".dom input").attr("placeholder"));
