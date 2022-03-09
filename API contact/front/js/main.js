2ec075c322cff29633338da593c604d17891d3de
alexb60 <alexandrebertrand60@yahoo.fr>
2022-03-09 11:20:25 +0100

    09 03 2022 11 20

--- API contact/front/js/main.js
+++ API contact/front/js/main.js
@@ -2,9 +2,50 @@ $("section").hide();
 
 $(document).on("click", "#liste-contacts", function (e) {
   e.preventDefault();
+  $("section").hide();
   $(".liste").show();
   liste();
 });
+
+$(document).on("click", "#nv-contact", function (e) {
+  e.preventDefault();
+  $("section").hide();
+  $(".ajout-contact").show();
+});
+
+$(document).on("submit", ".ajout-contact form", function (e) {
+  e.preventDefault();
+  ajoutContact();
+});
+
+$(document).on("click", "button.modif-contact", function () {
+  $("section").hide();
+  $("section.modif-contact").show();
+  modifContact($(this).attr("id"));
+});
+
+$(document).on("submit", ".modif-contact form", function (e) {
+  e.preventDefault();
+  majContact($(".modif-contact .maj-contact").attr("id"));
+});
+
+function majContact(id) {
+  let request = $.ajax({
+    type: "PUT",
+    url: `http://localhost:3000/contacts/${id}`,
+    data: { nom: $(".modif-contact #nom").val(), prenom: $(".modif-contact #prenom").val() },
+    dataType: "json",
+  });
+
+  request.done(function (response) {});
+
+  request.fail(function (http_error) {
+    let server_msg = http_error.responseText;
+    let code = http_error.status;
+    let code_label = http_error.statusText;
+    alert("Erreur " + code + " (" + code_label + ") : " + server_msg);
+  });
+}
 
 function liste() {
   let request = $.ajax({
@@ -54,3 +95,77 @@ function liste() {
     alert("Erreur " + code + " (" + code_label + ") : " + server_msg);
   });
 }
+
+function ajoutContact() {
+  let request = $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/contacts",
+    data: { id: new Date().getTime(), nom: $("#nom").val(), prenom: $("#prenom").val() },
+    dataType: "json",
+  });
+
+  request.done(function (response) {
+    liste();
+  });
+
+  request.fail(function (http_error) {
+    let server_msg = http_error.responseText;
+    let code = http_error.status;
+    let code_label = http_error.statusText;
+    alert("Erreur " + code + " (" + code_label + ") : " + server_msg);
+  });
+}
+
+function modifContact(id) {
+  let request = $.ajax({
+    type: "GET",
+    url: `http://localhost:3000/contacts/${id}`,
+    dataType: "json",
+  });
+
+  request.done(function (response) {
+    $(".modif-contact #nom").val(response.nom);
+    $(".modif-contact #prenom").val(response.prenom);
+    $(".maj-contact").attr("id", response.id);
+  });
+
+  request.fail(function (http_error) {
+    let server_msg = http_error.responseText;
+    let code = http_error.status;
+    let code_label = http_error.statusText;
+    alert("Erreur " + code + " (" + code_label + ") : " + server_msg);
+  });
+}
+
+// OBJET D'ORIGINE :
+/*
+{
+  "contacts": [
+    {
+      "id": "1644970279085",
+      "nom": "Rerum voluptates sed",
+      "prenom": "Rerum labore irure e"
+    },
+    {
+      "id": "1645140574309",
+      "nom": "Fugit impedit offi",
+      "prenom": "Impedit velit tempo"
+    },
+    {
+      "id": "1645141867947",
+      "nom": "hidri pp",
+      "prenom": "ryan pp"
+    },
+    {
+      "id": "1645143977964",
+      "nom": "test nom",
+      "prenom": "test prenom"
+    },
+    {
+      "id": "1645612509797",
+      "nom": "Et excepteur molesti",
+      "prenom": "Molestiae eos corru"
+    }
+  ]
+}
+*/
