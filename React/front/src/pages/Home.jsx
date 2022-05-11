3f74eed5b44f7140458013944772f321a107d50e
alexb60 <alexandrebertrand60@yahoo.fr>
2022-05-11 14:22:54 +0200

    form de recherche / listing / tri

--- React/front/src/pages/Home.jsx
+++ React/front/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Header from "../components/Header";
+import Recherche from "../components/Recherche";
+
 const Home = () => {
   return (
   <div>
+    <Header />
     <h1>page Home</h1>
+    <Recherche/>
   </div>
   );
 }
